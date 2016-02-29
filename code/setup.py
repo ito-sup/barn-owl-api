c096f4b49b0ca8d8f0965396759bc74d807729b5
ito <ito@supreme-system.com>
2016-02-29 18:37:19 +0900

    test

---
+++ code/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='code',
+      version='0.1',
+      description='test',
+      author='ito',
+      author='ito@supreme-system.com',
+      license='ito',
+      packages=['code'],
+      install_requires=['Django',],)
